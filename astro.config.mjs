e40c4015b9acd6b6f3dd76fe50ffcf68635fea1b
Marat Sarvarov <shtirlizc@gmail.com>
2026-08-24 21:37:55 +0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://shtirlizc.github.io",
+  base: "/vk-kids-landing",
+});
