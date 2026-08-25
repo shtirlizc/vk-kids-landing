a225ee13cb6eb074a1e9f9b11cbb544cc6e53b08
shtirlizc <shtirlizc@gmail.com>
2026-08-25 20:06:53 +0500

    footer

---
+++ src/nav.ts
@@ -0,0 +1,14 @@
+export const nav = [
+  {
+    href: "services",
+    label: "Возможности",
+  },
+  {
+    href: "channels",
+    label: "Каналы",
+  },
+  {
+    href: "faq",
+    label: "FAQ",
+  },
+];
